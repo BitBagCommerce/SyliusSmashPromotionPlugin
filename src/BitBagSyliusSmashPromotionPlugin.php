d40f3f6f1ef21ce7ecc260768fd150efabf6677f
Mikołaj Król <mikolaj.krol@bitbag.pl>
2019-07-05 20:36:52 +0200

    Initial commit

---
+++ src/BitBagSyliusSmashPromotionPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusSmashPromotionPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class BitBagSyliusSmashPromotionPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
